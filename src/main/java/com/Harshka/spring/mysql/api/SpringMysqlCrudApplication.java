b52a0e075c41598525b990fb0680b5285b2b43b6
Harshaka89 <buddhinathvitharana@gmail.com>
2021-08-29 13:01:05 +0200

    spring project

---
+++ src/main/java/com/Harshka/spring/mysql/api/SpringMysqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.Harshka.spring.mysql.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Harshka/spring/mysql/api/SpringMysqlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Harshka.spring.mysql.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
